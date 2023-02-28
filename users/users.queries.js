4edd4246bef10db0bbdf53d951d1eb140ecd224e
BEUMGOO KIM <captain_kbg@naver.com>
2023-02-28 23:33:13 +0900

    로그인

--- users/users.queries.js
+++ users/users.queries.js
@@ -3,9 +3,10 @@ import client from "../client";
 export default {
   Query: {
     seeProfile: async (_, { username }) => {
+      //findUnique는 Unique로 설정된 필드만 검색 한다.
       return await client.user.findUnique({
         where: {
-          username,
+          username
         },
       });
     }
